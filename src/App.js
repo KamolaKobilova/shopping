ff7fcc6e006a8d41d46646671abc5f60417864ab
“Kamola <kamola_8880@mail.ru>
2022-10-29 21:19:56 +0500

    components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Header from './containers/Header';
+import ProductListing from './containers/ProductListing';
+import ProductDetail from './containers/ProductDetail';
+
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+
+      <Header/> 
+       <Routes>  
+        <Route path='/' exact component={ProductListing}/>
+        <Route path='/product/:productId' exact component={ProductDetail}/>
+        <Route>404 Not Found</Route>
+       </Routes>
+    
+     
       </div>
-  )
+  );
 }
 
 export default App
