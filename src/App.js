a6eb7e65cddda8f974624c2d1d3e089d45222b51
“Kamola <kamola_8880@mail.ru>
2022-11-07 12:00:45 +0500

    router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router,Route, Routes, Switch} from 'react-router-dom';
-import Header from './containers/Header';
-import ProductListing from './containers/ProductListing';
-import ProductDetail from './containers/ProductDetail';
-import { useSelector } from 'react-redux';
-import ProductComponent from './containers/ProductComponent';
-
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Switch,
+} from "react-router-dom";
+import Header from "./containers/Header";
+import ProductListing from "./containers/ProductListing";
+import ProductDetail from "./containers/ProductDetail";
+import { useSelector } from "react-redux";
+import ProductComponent from "./containers/ProductComponent";
 
 function App() {
-  const state = useSelector((state) => state)
+  const state = useSelector((state) => state);
   console.log(state);
   return (
     <div className="App">
       <Routes>
-          <Route path="/" exact component={ProductListing} />
-          <Route path="component"component={ProductComponent} />
-          <Route path="/product/:productId" component={ProductDetail} />
-          <Route>404 Not Found!</Route>
+        <Route path="/" exact element={<ProductListing />} />
+        <Route path="component" element={<ProductComponent/>} />
+        <Route path="/product/:productId" element={<ProductDetail/>} />
+        <Route>404 Not Found!</Route>
       </Routes>
     </div>
   );
 }
 
-export default App
+export default App;
